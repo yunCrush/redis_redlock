adc304a1eda4f3cf3f5097643df08d54bc1b820a
yunCrush <yunfei1577266641@gmail.com>
2022-04-25 16:07:59 +0800

    redis redlock

---
+++ redis_redlock/src/main/java/com/yun/redis/redlock/RedisRedlockApplication.java
@@ -0,0 +1,15 @@
+package com.yun.redis.redlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisRedlockApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RedisRedlockApplication.class, args);
+    }
+
+}
